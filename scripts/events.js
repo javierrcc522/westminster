19412d86a684c047ae2ce4a6846eed3b5ddc702f
J <javi@Js-MacBook-Pro.local>
2020-01-20 13:19:39 -0800

    add calendar

---
+++ scripts/events.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: [ 'dayGrid', 'list' ],
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,listWeek'
+        },
+        editable: true,
+        eventLimit: true,
+        defaultDate: '2020-01-01',
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2020-01-01',
+                url: 'http://google.com/',
+            },
+            {
+                title: 'Long Event',
+                start: '2020-01-02',
+                end: '2020-01-03'
+            }
+        ]
+    });
+    calendar.render();
+
+});
